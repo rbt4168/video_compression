41011529bbd5469a0327a1ac3cc5fe57b66c7420
Waynexcellence <waynexcellence@gmail.com>
2024-12-22 21:14:27 +0800

    modify server client

--- client.py
+++ client.py
@@ -1,22 +1,22 @@
 import copy, time, requests
-from share import PERSON, ROOM
+from share import PERSON, DATA, ROOM
+from typing import List
 import cv2
+import numpy as np
+import queue
 import atexit
 import threading
 
-class PARAM():
-    def __init__(self , pos:tuple[int, int]=(0, 0)):
-        self.pos = pos
-
-def mouse_callback(event, x, y, flags, param:PARAM):
-    if event == cv2.EVENT_LBUTTONDOWN:  # 滑鼠按下
-        pass
-    elif event == cv2.EVENT_MOUSEMOVE:  # 滑鼠移動
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Error: Could not open webcam.")
+    exit(-1)
+def mouse_callback(event, x, y, flags, data:DATA):
+    if event == cv2.EVENT_MOUSEMOVE:  # 滑鼠移動
         if flags & cv2.EVENT_FLAG_LBUTTON:  # 按住的情況下
-            param.pos = (x, y)
-            pass
-    elif event == cv2.EVENT_LBUTTONUP:  # 滑鼠放開
-        pass
+            if x<0 or x>data.WH[0] or y<0 or y>data.WH[1]:
+                return
+            data.point = (x, y)
 
 def on_exit(url:str, user:PERSON):
     response = requests.post(f"{url}/bye" , json=user.to_dict())
@@ -24,9 +24,9 @@ def on_exit(url:str, user:PERSON):
         obj = PERSON.to_obj(response.json())
         print(obj.name)
 
-def enroll(url:str, width:int=400, height:int=400)->PERSON:
+def enroll(url:str)->PERSON:
     try:
-        user = PERSON(WH=(width, height)) 
+        user = PERSON() 
         while True:
             user.name = input("Enter Your name: ").strip()
             if len(user.name) > 0:
@@ -113,44 +113,164 @@ def search(url:str, myself:PERSON, myroom:ROOM)->tuple[ROOM, PERSON]:
         print("Request failed:", e)
     return myroom, myself
 
-def main():
-    cap = cv2.VideoCapture(0)
-    if not cap.isOpened():
-        print("Error: Could not open webcam.")
-        exit()
+def watch(url:str, mydata:DATA)->None:
+    global cap
     WIDTH = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     HEIGHT = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    # port = int(input("Enter the server port number: "))
-    url = f"http://127.0.0.1:6000"
+    mydata.WH = (WIDTH, HEIGHT)
+    try:
+        data = None
+        while True:
+            time.sleep(0.03)
+            ret, frame = cap.read()
+            if not ret:
+                print("Error: cap.read()")
+                exit(-1)
+            mydata.mosaic = to_mosaic(frame, grid=16, WIDTH=WIDTH, HEIGHT=HEIGHT)               # 全畫面 馬賽克
+            if data is not None:
+                mydata.detail , mydata.seen = part_frame(frame, data.point, 0.1)               # 對於 point 附近做細節
+            else:
+                mydata.detail , mydata.seen = part_frame(frame, (WIDTH//2, HEIGHT//2), 0.1)     # 對於 point 附近做 detail
+            response = requests.post(f"{url}/watch/{mydata.ID}", json=mydata.to_dict())
+            if response.status_code == 200:
+                data = DATA.to_obj(response.json())
+                if data.valid:
+                    if not cv2.getWindowProperty(data.name, cv2.WND_PROP_VISIBLE):
+                        cv2.destroyAllWindows()
+                        cv2.namedWindow(data.name, cv2.WINDOW_NORMAL)
+                        cv2.setMouseCallback(data.name, mouse_callback, mydata)                   # 其他人的data
+                    if data.mosaic.dtype != "uint8":                
+                        data.mosaic = cv2.normalize(data.mosaic, None, 0, 255, cv2.NORM_MINMAX) # type 要符合cv2
+                        data.mosaic = data.mosaic.astype("uint8")                               # type 要符合cv2
+                    ciasom = to_ciasom(data.mosaic, data.grid, data.WH[0], data.WH[1])          # 將馬賽克放大到對方原本的大小
+                    result = merge(ciasom, data.detail, data.WH, data.seen)                     # 將 detail 和 放大的馬賽克結合
+                    cv2.imshow(data.name, result)                                               # 展示 result
+                    window_ratio(data.name, WIDTH, HEIGHT)                                      # 讓畫面保持比例
+                    if cv2.waitKey(1) & 0xff == ord("q"):
+                        cv2.destroyWindow(data.name)
+                        print(f"close {data.name} window")
+                        exit(0)
+            else:
+                print("Error:", response.json())
+                break
+        return None
+    except requests.exceptions.RequestException as e:
+        print("Request failed:", e)
 
-    myself = enroll(url, WIDTH, HEIGHT)
-    room = ROOM()
+def window_ratio(window_name:str, WIDTH:int, HEIGHT:int):
+    cv2.waitKey(1)
+    window_size = cv2.getWindowImageRect(window_name)
+    if window_size:
+        _, _, current_width, current_height = window_size
+        aspect_ratio = WIDTH / HEIGHT
+        if current_width / current_height > aspect_ratio:
+            new_width = int(current_height * aspect_ratio)
+            new_height = current_height
+        else:
+            new_width = current_width
+            new_height = int(current_width / aspect_ratio)
+        cv2.resizeWindow(window_name, new_width, new_height)
+
+def part_frame(frame, point:tuple[int, int], side_ratio=0.1)->tuple[np.ndarray, tuple[tuple[int, int], tuple[int, int]]]:
+    frame_height, frame_width, channels = frame.shape
+    half_width = int(frame_width*side_ratio)
+    half_height = int(frame_height*side_ratio)
+
+    x1 = max(0, point[0] - half_width)
+    y1 = max(0, point[1] - half_height)
+    x2 = min(frame_width, point[0] + half_width)
+    y2 = min(frame_height, point[1] + half_height)
+
+    preserved_region = frame[y1:y2, x1:x2].copy()
+    return preserved_region, ((x1, y1), (x2, y2))
+
+def to_mosaic(frame, grid:int, WIDTH:int, HEIGHT:int):
+    compressed_height = HEIGHT // grid
+    compressed_width = WIDTH // grid
+    result = np.zeros((compressed_height, compressed_width, 3), dtype=np.uint8)
+    for i in range(compressed_height):
+        for j in range(compressed_width):
+            y_start = max(i*grid, 0)
+            y_end   = min((i+1)*grid, HEIGHT)
+            x_start = max(j*grid, 0)
+            x_end   = min((j+1)*grid, WIDTH)
+            block = frame[y_start:y_end, x_start:x_end]
+            avg_color = block.mean(axis=(0, 1)).astype(np.uint8)
+            result[i, j] = avg_color
+    return result
+
+def to_ciasom(mosaic, grid:int, large_width:int, large_height:int):
+    # mosaic_height, mosaic_width, _ = mosaic.shape
+    # result = np.zeros((large_height, large_width, 3), dtype=np.uint8)
+    # for i in range(mosaic_height):
+    #     for j in range(mosaic_width):
+    #         avg_color = mosaic[i, j]
+    #         y_start = i * grid
+    #         y_end = min((i + 1) * grid, large_height)
+    #         x_start = j * grid
+    #         x_end = min((j + 1) * grid, large_width)
+    #         result[y_start:y_end, x_start:x_end] = avg_color
+    mosaic_height, mosaic_width, _ = mosaic.shape
+    expanded = np.repeat(np.repeat(mosaic, grid, axis=0), grid, axis=1)
+    result = expanded[:large_height, :large_width]
+    return result
+
+def merge(mosaic, detail, WH:tuple[int, int], seen:tuple[tuple[int, int], tuple[int, int]]):
+    mosaic_height, mosaic_width, mosaic_channels = mosaic.shape
+    detail_height, detail_width, detail_channels = detail.shape
+    x1, y1 = seen[0]
+    x2, y2 = seen[1]
+    if mosaic_width != WH[0] or mosaic_height != WH[1]:
+        return mosaic
+    if mosaic_channels != detail_channels:
+        return mosaic
+    if (x2-x1) != detail_width or (y2-y1) != detail_height:
+        return mosaic
+    merged_image = mosaic.copy()
+    merged_image[y1:y2, x1:x2] = detail
+    return merged_image
+
+
+def main():
+    # cap = cv2.VideoCapture(0)
+    # if not cap.isOpened():
+    #     print("Error: Could not open webcam.")
+    #     exit()
+    # WIDTH = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    # HEIGHT = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    # port = int(input("Enter the server port number: "))
+    url = f"http://140.112.30.168:6000"
+
+    myself = enroll(url)
+    myroom = ROOM()
+    mydata = DATA(ID=myself.ID, name=myself.name, grid=16, valid=True)
     while True:
-        c = input("action")
-        if c=="c":
-            room, myself = create(url, myself, room)
-            print(f"in client")
-            print(f"myself = {myself}")
-            print(f"room = {room}")
-        elif c=="i":
-            room, myself = invite(url, myself, room)
-            print(f"in client")
-            print(f"myself = {myself}")
-            print(f"room = {room}")
-        elif c=="s":
-            room, myself = search(url, myself, room)
-            print(f"in client")
-            print(f"myself = {myself}")
-            print(f"room = {room}")
-        elif c=="q":
+        # print(f"{myself}")
+        # print(f"{myroom}")
+        c = input("action[ create/ invite/ search/ ok/ exit]: ")
+        if c=="create" or c=="c":
+            myroom, myself = create(url, myself, myroom)
+        elif c=="invite" or c=="i":
+            myroom, myself = invite(url, myself, myroom)
+        elif c=="search" or c=="s":
+            myroom, myself = search(url, myself, myroom)
+        elif c=="ok" or c=="o":
+            break
+        elif c=="exit" or c=="e":
+            return
+    
+    while True:
+        # ret, frame = cap.read()
+        # if not ret:
+        #     print("Error: cap.read()")
+        #     break
+        # mydata.mosaic = to_mosaic(frame, grid=16, WIDTH=WIDTH, HEIGHT=HEIGHT)
+        watch(url, mydata)
+        key = cv2.waitKey(1) & 0xff
+        if key == ord('q'):
             break
 
-
-    # param = PARAM()
-    # cv2.namedWindow("Mouse Interaction", cv2.WINDOW_NORMAL)
-    # cv2.setMouseCallback("Mouse Interaction", mouse_callback, param)
-
-
+        
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,10 +1,34 @@
 from flask import Flask, request, jsonify
 from share import PERSON, DATA, ROOM
+import threading, time
 import copy
+import numpy as np
 
 app = Flask(__name__)
 clients = [PERSON(ID=index) for index in range(8)]
-rooms = [ROOM() for index in range(4)]
+rooms = [ROOM(RID=index,ID=[], data=[], present=[]) for index in range(4)]
+
+fake = DATA(ID=-1,
+            name="fake",
+            index=-1,
+            grid=16,
+            WH=(640,480),
+            point=(320,240),
+            mosaic=np.array([
+                [[255, 0, 0], [0, 255, 0], [0, 0, 255]],  # Red, Green, Blue
+                [[255, 255, 0], [0, 255, 255], [255, 0, 255]],  # Yellow, Cyan, Magenta
+                [[192, 192, 192], [128, 128, 128], [64, 64, 64]]  # Light gray, gray, dark gray
+            ], dtype=np.uint8),
+            valid=True)
+
+def S10():
+    for index, client in enumerate(clients):
+        if client.valid:
+            print(f"clients[{index}] = {client}")
+    for index, room in enumerate(rooms):
+        if len(room.ID):
+            print(f"rooms[{index}] = {room}")
+    threading.Timer(3, S10).start()
 
 @app.route("/bye", methods=["POST"])
 def bye():
@@ -12,10 +36,14 @@ def bye():
     user = PERSON.to_obj(request.json)
     if user.ID<0 or user.ID>=8:
         return jsonify({"message": f"Invalid ID {user.ID}"}), 400
-    room = rooms[user.room]     # 如果正在視訊 是否 需要斷開連結
-    clients[user.ID] = PERSON()
-    user.name = "bye"
     print(f"clients[{user.ID}] clear")
+    if user.room:
+        room = rooms[user.room]     # 如果正在視訊 是否 需要斷開連結
+        # room.data.pop(user.index)
+        # room.present.pop(user.index)
+        # room.ID.pop(user.index)
+    clients[user.ID] = PERSON(ID=user.ID)
+    user.name = "bye"
     return jsonify(user.to_dict()), 200
 
 @app.route("/enroll", methods=["POST"])
@@ -23,85 +51,132 @@ def enroll():
     global clients, rooms
     user = PERSON.to_obj(request.json)
     for index, client in enumerate(clients):
-        if not user.valid and not client.valid:  # 還沒找到 and 這格沒有使用者佔走
+        if not user.valid and not client.valid: # 還沒找到 and 這格沒有使用者佔走
             user.ID = index
             user.valid = True
-        if client.name == user.name:             # handle once find repeat name
+        if client.name == user.name:            # handle once find repeat name
             return jsonify({"message": "The name has existed already."}), 400
-    if not user.valid:                           # no repeat name = no available quota
+    if not user.valid:                          # no repeat name = no available quota
         return jsonify({"message": "There is no available space for clients."}), 503
-    clients[user.ID].valid = user.valid
-    clients[user.ID].name = user.name
-    clients[user.ID].WH = user.WH
-    print(f"{user} has been enrolled.")
+    clients[user.ID] = user                     # 更新 server 端
+    print(f"{user}")
+    print(f"is enrolled successfully.")
     return jsonify(user.to_dict()), 200
 
 @app.route("/create/<int:room>", methods=["POST"])
-def create(room:int):
+def create(room: int):
     global clients, rooms
-    user = PERSON.to_obj(request.json)
-    print(f"{user} create room {room}")
-    if not user.room == None:
-        return jsonify({"message": "You have being in a room already."}), 400
-    if len(rooms[room].ID):
-        return jsonify({"message": "The room has been created already."}), 400
-    user.room = room
-    rooms[room].ID.append(user.ID)
-    rooms[room].data.append(DATA(ID=user.ID,index=0))
-    rooms[room].present.append(True)
-    return jsonify({"user":user.to_dict(), "room":rooms[room].to_dict()}), 200
+    try:
+        user = PERSON.to_obj(request.json)
+    except Exception as e:
+        print(f"Error parsing user: {e}")
+        return jsonify({"message": "Invalid user json"}), 400
+    if room < 0 or room >= len(rooms):
+        return jsonify({"message": "Invalid room ID"}), 400
+    if user.room is not None:
+        print(f"{user.name}.room != None")
+        return jsonify({"message": "You are already in a room."}), 400
+    if rooms[room].ID:
+        print(f"rooms[{room}].ID = {rooms[room].ID}")
+        return jsonify({"message": "The room has already been created."}), 400
+    # Update states
+    user.room = room                # 更新使用者房間編號
+    user.index = 0                  # 使用者是該房間的第一個 client
+    clients[user.ID] = user         # 更新 server 端
+    rooms[room].ID = [user.ID]      
+    rooms[room].data = [DATA(ID=user.ID, name=user.name, index=0)]
+    rooms[room].present = [True]
+    print(f"{user.name}")
+    print(f"create {room}")
+    print(f"successfully.")
+    # Return response
+    return jsonify({"user": user.to_dict(), "room": rooms[room].to_dict()}), 200
         
 @app.route("/invite/<Uname>", methods=["POST"])
-def invite(Uname:str):           # 被邀請人的 name, 邀請至哪一個 room
+def invite(Uname:str):                  # 被邀請人的 name, 邀請至使用者當前所在的 room
     global clients, rooms
-    user = PERSON.to_obj(request.json)
-    if user.room == None:
+    try:
+        user = PERSON.to_obj(request.json)
+    except Exception as e:
+        print(f"Error parsing user: {e}")
+        return jsonify({"message": "Invalid user json"}), 400
+    if user is None:
+        return jsonify({"message": "Invalid User json."}), 400
+    if user.room is None:
         return jsonify({"message": "You are not in any room."}), 400
-    print(f"{user.name} invites {Uname} into room {user.room}")
     if user.room<0 or user.room>=4:
         return jsonify({"message": "invalid Room ID"}), 400
+    print(f"{user.name} invites {Uname} into rooms[{user.room}]")
     room = rooms[user.room]
-    print(f"user = {user}")
-    print(f"room = {room}")
-    if user == None or room == None:
-        return jsonify({"message": "data missed"}), 400
     if user.ID not in room.ID:
-        return jsonify({"message": "No inviting privilege in this room."}), 400
+        return jsonify({"message": "No inviting privilege."}), 400
     for index, client in enumerate(clients):
         if client.valid and client.name == Uname:
-            UID = client.ID
+            invitee = client
             break
         if index == 7:
             return jsonify({"message": f"{Uname} not found"}), 404
-    print(f"user = {user}")
-    print(f"room = {room}")
-    
     amount = len(room.ID)
-    room.ID.append(UID)
-    print(f"{room.ID} append {UID}")
-    room.data.append(DATA(ID=UID,index=amount))
+    room.ID.append(invitee.ID)
+    room.data.append(DATA(ID=invitee.ID, name=invitee.name, index=amount))
     room.present.append(False)
+    print(f"{user}")
+    print(f"invite")
+    print(f"{invitee}")
+    print(f"to {room}")
+    print(f"successfully.")
     return jsonify({"user": user.to_dict(), "room": room.to_dict()}), 200
 
 @app.route("/search/<int:RID>", methods=["POST"])
 def search(RID:int):
     global clients, rooms
-    user = PERSON.to_obj(request.json)
-    print(f"{user} is searching for room id = {RID}")
+    try:
+        user = PERSON.to_obj(request.json)
+    except Exception as e:
+        print(f"Error parsing user: {e}")
+        return jsonify({"message": "Invalid user json"}), 400
+    if RID<0 or RID>=4:
+        return jsonify({"message": "Invalid Room ID"}), 400
     room = rooms[RID]
-    print(f"search list user id in room = {room.ID}")
-    if not user.ID in room.ID:          # 沒有在邀請名單上
+    print(f"{user}")
+    print(f"is searching for")
+    print(f"{room}")
+    if not user.ID in room.ID:              # 沒有在邀請名單上
         print(f"{user.name} didn't get into room {RID}")
         return jsonify({"message": "You have not been invited yet"}), 400
-    else:                               # 成功進入到 被邀請的房間
-        index = room.ID.index(user.ID)  # index = user 在該房間的順序
-        user.room = RID                 # 更新 user 的 room 編號
-        clients[user.ID] = user         # 
-        room.present[index] = True      # 房間第 index 個人出席
-        print(f"{user.name} enter room {RID}")
-        return jsonify({"user": user.to_dict(), "room": room.to_dict()}), 200
-        
+    index = room.ID.index(user.ID)          # index = user 在該房間的順序
+    user.room = RID                         # 更新 user 的 room 編號
+    user.index = index                      # 是該房間的第 index 個 client
+    clients[user.ID] = user                 # 更新 server 端
+    room.present[index] = True              # 房間第 index 個人出席
+    print(f"{user.name}")
+    print(f"enter {room}")
+    return jsonify({"user": user.to_dict(), "room": room.to_dict()}), 200
+
+@app.route("/watch/<int:ID>", methods=["POST"])
+def watch(ID:int):
+    global clients, rooms
+    client = clients[ID]
+    try:
+        data = DATA.to_obj(request.json)
+    except Exception as e:
+        print(f"Error parsing data: {e}")
+        return jsonify({"message": "Invalid data json"}), 400
+    try:
+        room = rooms[client.room]
+    except Exception as e:
+        print(f"Error Room ID: {e}")
+        return jsonify({"message": "Invalid Room ID"}), 400
+    room.data[client.index] = data      # 把 user 資料放到 room 裡面
+    if client.index==0:
+        if len(room.ID)>1:
+            return jsonify(room.data[1].to_dict()), 200
+        else:
+            return jsonify(data.to_dict()), 200
+    elif client.index==1:
+        return jsonify(room.data[0].to_dict()), 200
 
 if __name__ == "__main__":
     # port = int(input("Enter the port number for the server: "))
-    app.run(debug=True, port=6000)
+    S10()
+    app.run(port=6000)
